bc82c795a5f497359b18fdd80b907e40c094b588
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-02-28 10:40:52 +1300

    Responsive nav bar and to top button

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("connected 🙃");
+
+//variables for js
+const menuList = document.getElementById("menuList");
+let mybutton = document.getElementById("to-top");
+
+//nav bar responsive js
+menuList.style.maxHeight = "0px";
+
+function togglemenu() {
+  if (menuList.style.maxHeight === "0px") {
+    menuList.style.maxHeight = "300px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
+
+//To top button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
